e33c6077328cb8c30e184675ffd9afe0d788f82a
Marion Paolo Abeabe <mabeabe28@gmail.com>
2020-03-03 21:27:03 +0000

    Clean code tabs

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -8,7 +8,6 @@ const Items = (props) => {
   const itemDefaults = {id:'',name:'',quantity:'',price:'',diner:{}};
 
   const submitItems = () => {
-    
     //depending on quantity add however much
     const quantity = inputs.quantity;
     let item = [];
@@ -17,65 +16,58 @@ const Items = (props) => {
       item.push({id:(props.items.length + index),name:inputs.name,price:inputs.price,diner:{}});
     });
 
-    const newItems = _.concat([...props.items], item);
+	const newItems = _.concat([...props.items], item);
     
     //set new list
     props.setItems(newItems);
-
-  
   }
+
   const {inputs, handleInputChange, handleSubmit} = useForm(submitItems, itemDefaults, true);
-
-
   return(
     <React.Fragment>
-          <h3>Items</h3>
+        <h3>Items</h3>
 
-       <FadeIn className="item-list" key={props.items.length}>
-            {props.items.map((item, key) => {
-              //console.log(index);
-              return (<li key={key} 
-                id={item.id}
-                onClick={props.onChooseItem}
-              > 
-                {(item.diner.id !== undefined) ? (
-                  <div className={`item-person person-color-${parseFloat(item.diner.id)+1}`}>
-                      {item.diner.name.slice(0,1)}
-                  </div>
-                )
-                :
-                (
-                  <div className={`item-person person-color-none`}>
-                      U
-                  </div>
-                )}
-                <div className="item-info">
-                  {item.name} @ £{item.price}  
-                </div>
-
-                
-              </li>);
+       	<FadeIn className="item-list" key={props.items.length}>
+			{props.items
+			.map((item, key) => {
+				//console.log(index);
+				return (<li key={key} 
+					id={item.id}
+					onClick={props.onChooseItem}
+				> 
+					{(item.diner.id !== undefined) ? (
+						<div className={`item-person person-color-${parseFloat(item.diner.id)+1}`}>
+							{item.diner.name.slice(0,1)}
+						</div>
+						)
+						:
+						(
+						<div className={`item-person person-color-none`}>
+							U
+						</div>
+					)}
+					<div className="item-info">
+					{item.name} @ £{item.price}  
+					</div>
+              	</li>);
             })}
-          </FadeIn>
+        </FadeIn>
           
-          <form onSubmit={handleSubmit}>
-          <div className="form-group">
-              <input id="name" onChange={handleInputChange} type="text" value={inputs.name} placeholder="Item Name" required={true}/>
-            </div>
-
-            <div className="form-group">
-              <input id="price" onChange={handleInputChange} type="number" value={inputs.price} placeholder="Individual Price" required={true}/>
-            </div>
-
-            <div className="form-group">
-              <input id="quantity" onChange={handleInputChange} type="number" value={inputs.quantity} placeholder="Quantity" required={true}/>
-            </div>
-            <button type="submit">Add Item</button>
-          </form>
+		<form onSubmit={handleSubmit}>
+			<div className="form-group">
+				<input id="name" onChange={handleInputChange} type="text" value={inputs.name} placeholder="Item Name" required={true}/>
+			</div>
+			<div className="form-group">
+				<input id="price" onChange={handleInputChange} type="number" value={inputs.price} placeholder="Individual Price" required={true}/>
+			</div>
+			<div className="form-group">
+				<input id="quantity" onChange={handleInputChange} type="number" value={inputs.quantity} placeholder="Quantity" required={true}/>
+			</div>
+			<button type="submit">Add Item</button>
+		</form>
 
     </React.Fragment>
   )
-
 };
  
 export default Items;
